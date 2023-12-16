0912d8e4a4589d13b7edb8667a32c57760445701
Jianiqng Ma <jianqing997@gmail.com>
2023-12-16 23:30:17 +0800

    create basic pages and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
-export default function App() {
-    return <h1 className="text-red-500">App</h1>;
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/profile" element={<Profile />}></Route>
+                <Route path="/signin" element={<SignIn />}></Route>
+                <Route path="/signout" element={<SignUp />}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
+
+export default App;
